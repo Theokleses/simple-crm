295929e7a561be5653b32f5722ea79a9fc28d9d0
Theokleses <osahonschmolze222@gmail.com>
2025-10-18 15:43:54 +0200

    Added new DialogAddUser component with form fields and datepicker
    - generated new standalone component DialogAddUserComponent
    - imported required Angular Material modules (MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule)
    - created dialog template with multiple input fields including a datepicker
    - adjusted dialog window styling for better layout and spacing
    - imported MatNativeDateModule in UserComponent to fix DateAdapter issue for datepicker

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {DialogAddUserComponent} from '../dialog-add-user/dialog-add-user.component';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
