93710259061f47d9fd54a224f1e4558893d2161e
Theokleses <osahonschmolze222@gmail.com>
2025-10-20 17:45:07 +0200

    Integrate Firebase into the project and connect Firestore. Configured Firebase in app.config.ts and imported required modules. Implemented Firestore integration in DialogAddUserComponent to save user data as JSON. Added data conversion using toJSON / spread operator before saving to Firestore.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'simple-crm-ed655',
+        appId: '1:805053212381:web:42420641dee8cabb809d34',
+        storageBucket: 'simple-crm-ed655.firebasestorage.app',
+        apiKey: 'AIzaSyB1Pvh92OiLFF17wmldbK1g0_NoiGeHk6s',
+        authDomain: 'simple-crm-ed655.firebaseapp.com',
+        messagingSenderId: '805053212381',
+        // projectNumber: '805053212381',
+        // version: '2',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -6,7 +6,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {User} from '../../models/user.class';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {Firestore, collection, addDoc} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -19,11 +20,19 @@ export class DialogAddUserComponent {
 
   user = new User();
   birthDate: Date = new Date();
+  firestore = inject(Firestore);
+  
 
 
-  saveUser() {
+  async saveUser() {
     this.user.birthDate = this.birthDate.getTime();
-    console.log('Current user is', this.user); 
-  }  
+    console.log('Current user is', this.user);
 
+    let usersCollection = collection(this.firestore, 'users');
+    // await addDoc(usersCollection, this.user);
+    await addDoc(usersCollection, this.user.toJSON());
+
+
+    console.log('User successfully added to Firestore!');
+  }
 }
