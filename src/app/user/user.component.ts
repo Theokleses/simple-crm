da3454a6bea4ea48dd8535883d4ed2078a85292e
Theokleses <osahonschmolze222@gmail.com>
2025-10-22 14:18:32 +0200

    Subscribe to Firestore database using Observable and display user data in the HTML table. Bound Firestore collection to the template for real-time updates of user information.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,28 +1,42 @@
-import {Component, inject} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {DialogAddUserComponent} from '../dialog-add-user/dialog-add-user.component';
-import {MatNativeDateModule} from '@angular/material/core';
-import {User} from '../../models/user.class';
-import {MatCardModule} from '@angular/material/card';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule, MatCardModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatCardModule,
+    AsyncPipe,
+  ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
-  user = new User();
-
+  users$: Observable<User[]>;
   private dialog = inject(MatDialog);
+  firestore = inject(Firestore);
+
+  constructor() {
+    const usersCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(usersCollection, {idField: 'id',}) as Observable<User[]>;
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
-
 }
