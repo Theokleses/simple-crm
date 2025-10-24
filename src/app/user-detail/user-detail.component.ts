c11d8266700742dd6f9aabb494c30ed0bc70460f
Theokleses <osahonschmolze222@gmail.com>
2025-10-24 16:21:25 +0200

    Inject ActivatedRoute service properly in UserDetailComponent

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
+  route = inject(ActivatedRoute);
 
+  constructor() {
+    console.log('Current user ID:', this.route.snapshot.paramMap.get('id'));
+  }
 }
