fa5b9d4c8e211f6a36f1153957426686914f50f1
Theokleses <osahonschmolze222@gmail.com>
2025-10-27 13:07:08 +0100

    Add user detail view to display selected user info and address using Firestore data

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,8 +14,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent {
   route = inject(ActivatedRoute);
+  firestore = inject(Firestore);
+
+  user$?: Observable<User>;
+  userId: string | null;
+  user: User = new User();
 
   constructor() {
-    console.log('Current user ID:', this.route.snapshot.paramMap.get('id'));
+    // console.log('Current user ID:', this.route.snapshot.paramMap.get('id'));
+    this.userId = this.route.snapshot.paramMap.get('id');
+    if (this.userId) {
+      const userDocRef = doc(this.firestore, `users/${this.userId}`);
+      this.user$ = docData(userDocRef, { idField: 'id' }) as Observable<User>;
+      this.user$.subscribe((userData) => {
+        this.user = new User(userData);
+        console.log('Retrieved user data:', userData);
+      });
+    }
   }
 }
