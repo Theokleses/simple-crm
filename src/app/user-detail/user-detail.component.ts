69fe9aeb4c07c8e1cf757ed3ddb7d496dac3c172
Theokleses <osahonschmolze222@gmail.com>
2025-10-28 13:37:37 +0100

    Fix edit dialogs by importing CommonModule and creating deep user copy with toJSON() to prevent instant data changes

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -42,11 +42,11 @@ export class UserDetailComponent {
 
   editUserDetail(){
     const dialog = this.dialog.open(DialogEditUserComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 
   editMenu(){
     const dialog = this.dialog.open(DialogEditAddressComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 }
